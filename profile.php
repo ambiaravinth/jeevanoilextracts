a7017a1ac04fe2ac3a8f0f8178e0977bd850a988
niresh2001 <nnireshnallasamy@gmail.com>
2022-06-21 23:31:18 +0530

    niresh

--- profile.php
+++ profile.php
@@ -74,6 +74,122 @@
 
 
 
+
+<!-- PROFILE FORM -->
+
+
+
+
+<div class="row" style="margin-left:20%;">
+      <div class="col-6 offset-3">
+        <div class="container mt-5 card bg-white p-3">
+          <form class="signup-form" action="/register" method="post">
+   
+            <div class="row">
+            <div class="col-md-4">
+              <label for="fname" class="form-label">Username :</label>
+              <input type="text" class="form-control" id="fname" name="user_name" />
+            </div>
+            <div class="col-md-4">
+              <label for="name" class="form-label">Password :</label>
+              <input
+                type="text"
+                class="form-control"
+                id="name"
+                name="password"
+              />
+            </div>
+        </div>
+
+        
+
+
+
+
+		<div class="row" style="margin-top:2%;" >
+            <div class="col-md-4">
+              <label for="names" class="form-label">Name:</label>
+              <input type="text" class="form-control" id="fname" name="name1" />
+            </div>
+            <div class="col-md-4">
+              <label for="pan" class="form-label">PAN NO :</label>
+              <input
+                type="text"
+                class="form-control"
+                id="pan_no"
+                name="pan"
+              />
+            </div>
+        </div>
+
+
+		<div class="row" style="margin-top:2%;">
+            <div class="col-md-4">
+              <label for="email" class="form-label">Email :</label>
+              <input
+                type="email"
+                class="form-control"
+                id="email"
+                placeholder="abc@gmail.com"
+                name="Email_ID"
+              />
+            </div>
+
+
+			<div class="col-md-4">
+              <label for="name" class="form-label">GST NO:</label>
+              <input
+                type="text"
+                class="form-control"
+                id="gst"
+                name="gstno"
+              />
+            </div>
+            </div>
+   
+            <div class="row" style="margin-top:2%;">
+            
+       
+            <div class="col-md-4">
+              <label for="mobile" class="form-label">Mobile number :</label>
+              <input
+                type="text"
+                class="form-control"
+                id="mobile"
+                name="Mobile_No"
+              />
+            </div>
+
+
+			<div class="col-md-4">
+              <label for="address" class="form-label">Address :</label>
+              <textarea
+                type="textarea"
+                class="form-control"
+                id="address"
+                name="adrs"
+              ></textarea>
+            </div>
+
+
+
+        </div>
+           
+         
+            
+
+            <div class="col-12" style="margin-top:2%; margin-left:15% ">
+                <br>
+              <button type="cancel" onclick="javascript:window.location='profile.php';" class="btn btn-primary">Edit</button>
+			  <button type="submit" class="btn btn-primary" style="margin-left:25%;">save</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+
+
 <!-- CALL TO ACTION =============================-->
 <section class="content-block" style="background-color:#00bba7;">
 <div class="container text-center">
